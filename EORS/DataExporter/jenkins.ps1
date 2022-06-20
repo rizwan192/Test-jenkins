d53a4dd18aa8b4812079e428b866ba0378ffd0a0
Rizwan <rizwanulratul.192@gmail.com>
2022-06-20 23:34:48 +0600

    copy old logs

--- EORS/DataExporter/jenkins.ps1
+++ EORS/DataExporter/jenkins.ps1
@@ -5,45 +5,78 @@ $SecurePassword = $env:PASSWORD | ConvertTo-SecureString -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $env:USERNAME, $SecurePassword
 
 # Stop EORSDataExporterService
-Write-Host "Stopping Service"
+Write-Host "1. Stopping Service"
 $stopDataExporter = "`$dataExporterSer = Get-Service EORSDataExporterService -ErrorAction SilentlyContinue; if (!`$dataExporterSer.HasExited) { write-host `"EORSDataExporterService is running - stopping Service`"; `$dataExporterSer | Stop-Service -Force }"
 $EORSDataExporterScriptStop = [scriptblock]::Create($stopDataExporter)
 Invoke-command -computer $env:SERVERNAME -Credential $cred -ScriptBlock $EORSDataExporterScriptStop
 
 # Run RemoveEORSDataExporterService.bat file
-Write-Host "Running remove bat Service"
+Write-Host "2. Running remove bat Service"
 $runRemove = "cd; cd D:\CHART\EorsDataExporter\InstallScripts;ls; cmd /c RemoveEorsDataExpoterService.bat"
 $runRemoveScript = [scriptblock]::Create($runRemove)
 Invoke-command -computer $env:SERVERNAME -Credential $cred -ErrorAction Stop -ScriptBlock $runRemoveScript
 
+# Copy log folder from D:\CHART\EorsDataExporter\EorsDataExporterService\Logs
+Write-Host "3. Copy log folder from D:Chart"
+$runCopyLog = "cd; Copy-Item -Force -Recurse 'D:\CHART\EorsDataExporter\EorsDataExporterService\Logs\*'; -Destination 'D:\CHART\Logs'"
+$runCopyLogScript = [scriptblock]::Create($runCopyLog)
+Invoke-command -computer $env:SERVERNAME -Credential $cred -ErrorAction Stop -ScriptBlock $runCopyLogScript
+
 #remove old data from D:chart
-Write-Host "Removing old data from D:\chart"
+Write-Host "4. Removing old data from D:\chart"
 $removeFolders = " cd; Remove-Item D:\CHART\EorsDataExporter\EorsDataExporterService -Recurse -Force"
 $runRemoveFolders = [scriptblock]::Create($removeFolders)
 Invoke-command -computer $env:SERVERNAME -Credential $cred -ErrorAction Stop -ScriptBlock $runRemoveFolders
 
 #Copy new dataexporter.zip to destination server
-Write-Host "Copy new dataexporter.zip to destination server"
+Write-Host "5. Copy new dataexporter.zip to destination server"
 $session = New-PSSession -ComputerName $env:SERVERNAME -Credential $cred
 Copy-Item "$env:WORKSPACE\Package-Output\dataexporter.zip" -Destination "D:\CHART\" -ToSession $session -Recurse -Force
 
-# Unzip dataExport.zip, then remove installScripts and .zip file 
-Write-Host "Unzip dataExport.zip"
-$unzip = "Expand-Archive D:\CHART\dataexporter.zip -DestinationPath D:\CHART\ -Force; Remove-Item D:\CHART\dataexporter.zip -Recurse -Force; Remove-Item D:\CHART\InstallScripts -Recurse -Force ;Copy-Item 'D:\CHART\EORSDataExporterService' -Destination 'D:\CHART\EorsDataExporterService\' -Recurse -Force'; Rename-Item D:\CHART\EorsDataExporter\EORSDataExporterService D:\CHART\EorsDataExporter\EorsDataExporterService "
+# Unzip dataExport.zip
+Write-Host "6. Unzip dataExport.zip"
+$unzip = "Expand-Archive D:\CHART\dataexporter.zip -DestinationPath D:\CHART\ -Force"
 $unzipScript = [scriptblock]::Create($unzip)
 Invoke-command -computer $env:SERVERNAME -Credential $cred -ErrorAction Stop -ScriptBlock $unzipScript
 
+
+# Remove dataexporter.zip file and InstallScripts after extraction
+Write-Host "7. Remove Zip file after extraction"
+$removeZip = "Remove-Item D:\CHART\dataexporter.zip -Recurse -Force; Remove-Item D:\CHART\InstallScripts -Recurse -Force;"
+$removeZipScript = [scriptblock]::Create($removeZip)
+Invoke-command -computer $env:SERVERNAME -Credential $cred -ErrorAction Stop -ScriptBlock $removeZipScript
+
+# Copy extracted files from EORSDataExporterService to destination
+Write-Host "8. Copy extracted files to destination"
+$copyExtracted = "mkdir D:\CHART\EorsDataExporter\EorsDataExporterService; Copy-Item -Force -Recurse 'D:\CHART\EORSDataExporterService\*' -Destination 'D:\CHART\EorsDataExporter\EorsDataExporterService'"
+$copyExtractedScript = [scriptblock]::Create($copyExtracted)
+Invoke-command -computer $env:SERVERNAME -Credential $cred -ErrorAction Stop -ScriptBlock $copyExtractedScript
+
+# Paste log folder to D:\CHART\EorsDataExporter\EorsDataExporterService\Logs
+Write-Host "9. Copy log folder from D:Chart"
+$runPasteLog = "cd; Copy-Item -Force -Recurse 'D:\CHART\Logs\*' -Destination 'D:\CHART\EorsDataExporter\EorsDataExporterService\Logs'; Remove-Item -Path 'D:\CHART\Logs' -Recurse -Force"
+$runPasteLogScript = [scriptblock]::Create($runPasteLog)
+Invoke-command -computer $env:SERVERNAME -Credential $cred -ErrorAction Stop -ScriptBlock $runPasteLogScript
+
+# Delete the files from dataexport.zip
+Write-Host "10. Delete the files from dataexport.zip"
+$removeFolder = "Remove-Item -Path 'D:\CHART\EORSDataExporterService' -Recurse -Force"
+$removeFolderScript = [scriptblock]::Create($removeFolder)
+Invoke-command -computer $env:SERVERNAME -Credential $cred -ErrorAction Stop -ScriptBlock $removeFolderScript
+
+
 # Run RegisterEorsDataExpoterService file
-Write-Host "Run RegisterEorsDataExpoterService file"
+Write-Host "11. Run RegisterEorsDataExpoterService file"
 $SERVER = [string]$env:SERVERNAME
 $APP_NAME =  $SERVER.Substring(0,$SERVER.Length-1)
-$APP_NAME = $APP_NAME+$env:CHART_APP+"app"
-$runRegister = "cd; cd D:\CHART\EorsDataExporter\InstallScripts;ls; cmd /c RegisterEorsDataExpoterService.bat `$APP_NAME"
+$APP_NAME = $APP_NAME+"app"
+Write-Host $APP_NAME
+$runRegister = "cd; cd D:\CHART\EorsDataExporter\InstallScripts; cmd /c RegisterEorsDataExporterService.bat $APP_NAME"
 $runRegisterScript = [scriptblock]::Create($runRegister)
 Invoke-command -computer $env:SERVERNAME -Credential $cred -ErrorAction Stop -ScriptBlock $runRegisterScript
 
 # Start EORSDataExporterService
-Write-Host "Start EORSDataExporterService"
-$startDataExporter = "`$dataExporterSer = Get-Service EORSDataExporterService -ErrorAction SilentlyContinue; `$dataExporterSer | Start-Service "
+Write-Host "12. Start EORSDataExporterService"
+$startDataExporter = "`$dataExporterSer = Get-Service EORSDataExporterService -ErrorAction SilentlyContinue; `$dataExporterSer | Start-Service"
 $EORSDataExporterScriptStart = [scriptblock]::Create($startDataExporter)
 Invoke-command -computer $env:SERVERNAME -Credential $cred -ScriptBlock $EORSDataExporterScriptStart
